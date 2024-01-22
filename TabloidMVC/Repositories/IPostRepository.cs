985f56923771cebf49a87105d5e848f7fc29a761
Hannah Thompson <hannahmthompsonedu@gmail.com>
2024-01-22 10:08:14 -0500

    Delete Post Functionality

--- TabloidMVC/Repositories/IPostRepository.cs
+++ TabloidMVC/Repositories/IPostRepository.cs
@@ -5,8 +5,8 @@ namespace TabloidMVC.Repositories
     public interface IPostRepository
     {
         void Add(Post post);
-
         void EditPost(Post post);
+        void DeletePost(int id);
         List<Post> GetAllPublishedPosts();
         Post GetPublishedPostById(int id);
         Post GetUserPostById(int id, int userProfileId);
